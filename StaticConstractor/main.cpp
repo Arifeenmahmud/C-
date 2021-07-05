bf11ab4b3b449f95bc360ce99c4dac0890fe65bf
Arifeen Mahmud <arifeen.mahmud@yahoo.com>
2021-07-05 14:16:55 +0600

    Using Constructor  Static variable.

--- StaticConstractor/main.cpp
+++ StaticConstractor/main.cpp
@@ -7,8 +7,25 @@
 
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
-}
+using namespace std;
+
+    class widget {
+    public:
+    widget() {count++; }
+    ~widget() {--count; }
+    static int count;
+    };
+    int widget::count = 0;
+
+        int main() {
+        widget w,x;
+            std::cout <<"Now thereare " << w.count <<"widgets\n";
+        { widget w,x,y,z;
+            std::cout <<"Now thereare " << z.count <<"widgets\n";
+        }
+
+            std::cout <<"Now thereare " << w.count <<"widgets\n";
+        widget y;
+            std::cout <<"Now thereare " << w.count <<"widgets\n";
+        }
+
