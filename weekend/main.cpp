8b8e657ecdb8664b7572dcb6dd86d7676383e427
Arifeen Mahmud <arifeen.mahmud@gmail.com>
2021-07-10 01:38:29 +0600

    Initial Commit

---
+++ weekend/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  weekend
+//
+//  Created by Arifeen Mahmud on 10/7/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
