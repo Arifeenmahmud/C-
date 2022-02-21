001f8c0785ff43bb46cba334f85ee4ca8f97730f
Arifeen Mahmud <arifeen.mahmud@gmail.com>
2022-02-21 19:53:56 +0600

    Initial Commit

---
+++ DefultConstractor/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  DefultConstractor
+//
+//  Created by Arifeen Mahmud on 21/2/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
