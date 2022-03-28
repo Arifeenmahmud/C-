7e6265d8bad248f435b620fd3ba3397ba3dc007b
Arifeen Mahmud <arifeen.mahmud@gmail.com>
2022-03-29 01:44:23 +0600

    Weekend

--- weekend/main.cpp
+++ weekend/main.cpp
@@ -6,9 +6,37 @@
 //
 
 #include <iostream>
+using namespace std;
+class mess
+{
+public:
+ enum days{sat, sun, mon,tue,wed,thurs,fri};
+ int status;
+ void setStatus(int stat)
+ {
+ status=stat;
+ }
+ void show()
+ {
+ if(status==1)
+ cout<<"weekend"<<endl;
+ else if(status==0)
+ cout<<"Working day"<<endl;
+ else cout<<"Wrong input";
+ }
+};
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+int main()
+{
+ mess m1;
+ int day;
+ cin>> day;
+    cout<< m1.sat;
+ if ((day==m1.sat)||(day==m1.fri))
+ m1.setStatus(1);
+ else if
+((day==m1.sun)||(day==m1.mon)||(day==m1.tue)||(day==m1.wed)||(day==m1.thurs))
+ m1.setStatus(0);
+ m1.show();
+ return 0;
 }
